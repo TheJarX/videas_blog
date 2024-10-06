c35a0a52fd2cd62497b499c78d3c6968addadd52
Gerard Altamirano <hello@itsgerard.com>
2024-10-06 22:44:02 +0200

    fix: Date format in post card

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,8 @@ pub mod templates {
     pub struct NotFoundTemplate {}
 
     pub mod filters {
+        use chrono::{DateTime, NaiveDateTime, Utc};
+
         pub fn join_options(values: &Vec<Option<String>>, sep: &str) -> askama::Result<String>{
             let joined = values.into_iter()
                 .filter_map(|opt| opt.as_ref())
@@ -18,6 +20,11 @@ pub mod templates {
                 .collect::<Vec<&str>>()
                 .join(sep);
             Ok(joined)
+        }
+
+        pub fn parse_and_format_date(naive_date: &NaiveDateTime) ->askama::Result<String> {
+            let datetime_utc = DateTime::<Utc>::from_naive_utc_and_offset(*naive_date, Utc);
+            Ok(datetime_utc.format("%d.%m.%Y").to_string())
         }
     }
 }
